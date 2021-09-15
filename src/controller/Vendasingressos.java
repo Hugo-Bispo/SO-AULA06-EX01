a4f4a276d78490839a4d1b57a2f57698c5ff6c5b
Gugo <Gugo@PC-HG01>
2021-09-14 21:38:33 -0300

    V 1.0

---
+++ src/controller/Vendasingressos.java
@@ -0,0 +1,56 @@
+package controller;
+import java.util.concurrent.Semaphore;
+
+public class Vendasingressos extends  Thread{
+		private int usuarios;
+		private int ingressos;
+		private Semaphore semaphore;
+		
+	public Vendasingressos(int usuarios, int ingressos, Semaphore semaphore) {
+		this.usuarios= usuarios;
+		this.ingressos =ingressos;
+		this.semaphore = semaphore;
+	}
+	
+	public void run() {
+
+		usuarios = login(usuarios);
+		usuarios = processocompra(usuarios);
+		
+	}
+
+	private int login(int usuarios) {
+
+		int tempo = (int)((Math.random() * 2000) + 50);
+		System.out.println(tempo);
+		int aux = 0;
+		try {
+			sleep(tempo);
+			if(tempo>3000) {
+				System.err.println(usuarios + " ==>Timeout");
+			}else {
+				aux = usuarios;
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return aux;
+	}
+	
+	private int processocompra(int usuarios) {
+		int tempo = (int)((Math.random() * 3000) + 1000);
+		int aux=0;
+		System.err.println(tempo);
+		try {
+			sleep(tempo);
+			if(tempo>5500) {
+				System.err.println(usuarios + "Timeout");
+			}else {
+				aux = usuarios;
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return aux;
+	}
+}
